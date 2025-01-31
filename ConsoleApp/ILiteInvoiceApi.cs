fb107e1a4682ba56479e81eaddb753fbc1ac190c
Adam O'Neil <adamosoftware@gmail.com>
2025-01-31 13:13:15 -0500

    progress on console app

--- ConsoleApp/ILiteInvoiceApi.cs
+++ ConsoleApp/ILiteInvoiceApi.cs
@@ -1,7 +1,31 @@
-﻿namespace ConsoleApp;
+﻿using LiteInvoice.Database;
+using Refit;
 
+namespace ConsoleApp;
 
 internal interface ILiteInvoiceApi
 {
-//	Task<>
+	[Get("/api/businesses")]
+	Task<Business[]> GetMyBusinessesAsync([Header("ApiKey")] string apiKey);
+
+	[Post("/api/businesses")]
+	Task<Business> AddBusinessAsync([Header("ApiKey")]string apiKey, [Body]Business business);
+
+	[Put("/api/businesses/{id}")]
+	Task<Business> UpdateBusinessAsync([Header("ApiKey")]string apiKey, int id, [Body]Business business);
+
+	[Delete("/api/businesses/{id}")]
+	Task DeleteBusinessAsync([Header("ApiKey")]string apiKey, int id);
+
+	[Get("/api/customers")]
+	Task<Customer> GetMyCustomersAsync([Header("ApiKey")]string apiKey);
+
+	[Post("/api/customers")]
+	Task<Customer> AddCustomerAsync([Header("ApiKey")]string apiKey, [Body]Customer customer);
+
+	[Put("/api/customers/{id}")]
+	Task<Customer> UpdateCustomerAsync([Header("ApiKey")]string apiKey, int id, [Body] Customer customer);
+
+	[Delete("/api/customers/{id}")]
+	Task DeleteCustomerAsync([Header("ApiKey")] string apiKey, int id);
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp;
+using Microsoft.Extensions.Configuration;
+using Refit;
+
+var config = new ConfigurationBuilder()
+	.AddUserSecrets("254fb8d5-db60-4051-b43f-a4ae681fb10f")
+	.AddJsonFile("appsettings.json")
+	.Build();
+
+var api = RestService.For<ILiteInvoiceApi>(config["BaseUrl"]);
+var apiKey = config["MyApiKey"];
+
+var businesses = await api.GetMyBusinessesAsync(apiKey);
+
+foreach (var b in businesses) Console.WriteLine(b.Name);
+
+Console.ReadLine();
