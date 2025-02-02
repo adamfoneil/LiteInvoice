9d896e5cf50b98c5c63b2b2bfd21bf0f27d47f4e
Adam O'Neil <adamosoftware@gmail.com>
2025-02-02 11:29:10 -0500

    cleanup

--- RazorPagesApp/Pages/Business.cshtml.cs
+++ RazorPagesApp/Pages/Business.cshtml.cs
@@ -1,6 +1,5 @@
 using LiteInvoice.Database;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
