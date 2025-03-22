b73edf453408dbae4f9f4b6b35fd84c48b0671e2
Adam O'Neil <adamosoftware@gmail.com>
2025-03-22 15:50:57 -0400

    receivable basics

--- LiteInvoice.Database/ApplicationDbContext_Invoicing.cs
+++ LiteInvoice.Database/ApplicationDbContext_Invoicing.cs
@@ -1,10 +1,36 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace LiteInvoice.Database;
 
 public partial class ApplicationDbContext
 {
+	public async Task<(HoursEntry[] Hours, ExpenseEntry[] Expenses, decimal Total)> GetInvoiceDataAsync(int projectId)
+	{
+		var hours = await Hours
+			.Where(row => row.ProjectId == projectId && row.AddToInvoice)
+			.ToArrayAsync();
+
+		foreach (var item in hours) item.Project = null!;		
+
+		var expenses = await Expenses
+			.Where(row => row.ProjectId == projectId && row.AddToInvoice)
+			.ToArrayAsync();
+
+		foreach (var item in expenses) item.Project = null!;
+
+		var amount = hours.Sum(row => row.Hours * row.Rate) + expenses.Sum(row => row.Amount);
+
+		return (hours, expenses, amount);
+	}
+
+	private static JsonSerializerOptions DefaultSettings => new()
+	{
+		WriteIndented = true,
+		ReferenceHandler = ReferenceHandler.IgnoreCycles
+	};
+
 	public async Task<Invoice> CreateInvoiceAsync(int projectId, string? description = null)
 	{
 		ChangeTracker.Clear();
@@ -15,18 +41,16 @@ public partial class ApplicationDbContext
 			.Where(p => p.Id == projectId)
 			.SingleOrDefaultAsync() ?? throw new Exception("Project not found");
 
-		var hours = await Hours.Where(row => row.ProjectId == projectId && row.AddToInvoice).ToArrayAsync();
-		var expenses = await Expenses.Where(row => row.ProjectId == projectId && row.AddToInvoice).ToArrayAsync();
-		var amount = hours.Sum(row => row.Hours * row.Rate) + expenses.Sum(row => row.Amount);
+		var (hours, expenses, amount) = await GetInvoiceDataAsync(projectId);
 
 		var invoice = new Invoice()
 		{
 			Number = project.Customer.Business.NextInvoiceNumber,
 			ProjectId = projectId,
 			AmountDue = amount,
-			Date = DateTime.Now,
+			Date = DateTime.UtcNow,
 			Description = description,
-			Data = JsonSerializer.Serialize(new InvoiceData() { Hours = hours, Expenses = expenses })
+			Data = JsonSerializer.Serialize(new InvoiceData() { Hours = hours, Expenses = expenses }, DefaultSettings)
 		};
 
 		project.Customer.Business.NextInvoiceNumber++;
@@ -45,8 +69,12 @@ public partial class ApplicationDbContext
 		var invoice = await Invoices.SingleOrDefaultAsync(row => row.Id == invoiceId) ?? throw new Exception("Invoice not found");
 		var data = JsonSerializer.Deserialize<InvoiceData>(invoice.Data) ?? throw new Exception("Couldn't deserialize invoice data");
 
-		Hours.AddRange(data.Hours);
-		Expenses.AddRange(data.Expenses);
+		// ensure inserts
+		foreach (var item in data.Hours) item.Id = 0;
+		foreach (var item in data.Expenses) item.Id = 0;
+
+		Hours.UpdateRange(data.Hours);
+		Expenses.UpdateRange(data.Expenses);
 		Invoices.Remove(invoice);
 		await SaveChangesAsync();
 	}
