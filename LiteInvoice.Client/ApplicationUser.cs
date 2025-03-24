2cd134fd8b1916b5fb0ccfe067022864db1662b8
Adam O'Neil <adamosoftware@gmail.com>
2025-03-24 15:24:26 -0400

    removed obsolete stuff

--- LiteInvoice.Client/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LiteInvoice.Database;
-
-public class ApplicationUser
-{
-	public int UserId { get; set; }
-	public string? TimeZoneId { get; set; }
-
-	public ICollection<Business> Businesses { get; set; } = [];
-}
--- RazorPagesApp/ClaimsProfileExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Claims;
-
-namespace RazorPagesApp;
-
-internal static class ClaimsProfileExtensions
-{
-	public static int UserId(this ClaimsPrincipal claimsPrincipal) =>
-		int.Parse(claimsPrincipal.FindFirst(claim => claim.Type == ApplicationUserManager.UserIdClaimType)!.Value);
-}
