98889cb24e81473ef4c10bee117a11c31cf837e8
Adam O'Neil <adamosoftware@gmail.com>
2025-03-27 13:01:51 -0400

    customer address fields

--- LiteInvoice.Database/Customer.cs
+++ LiteInvoice.Database/Customer.cs
@@ -29,9 +29,12 @@ public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
 	{
 		builder.HasIndex(e => new { e.BusinessId, e.Name }).IsUnique();
 		builder.HasOne(e => e.Business).WithMany(e => e.Customers).HasForeignKey(e => e.BusinessId).OnDelete(DeleteBehavior.Restrict);
+		builder.Property(e => e.Address).HasMaxLength(100);
 		builder.Property(e => e.Name).HasMaxLength(100);
 		builder.Property(e => e.Contact).HasMaxLength(100);
 		builder.Property(e => e.Email).HasMaxLength(100);
 		builder.Property(e => e.Phone).HasMaxLength(100);
+		builder.Property(e => e.Zip).HasMaxLength(20);
+		builder.Property(e => e.State).HasMaxLength(2);
 	}
 }
---
+++ LiteInvoice.Database/Migrations/20250327165952_CustomerFieldLengths.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class CustomerFieldLengths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Zip",
+                table: "Customers",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Customers",
+                type: "character varying(2)",
+                maxLength: 2,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Zip",
+                table: "Customers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Customers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2)",
+                oldMaxLength: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
