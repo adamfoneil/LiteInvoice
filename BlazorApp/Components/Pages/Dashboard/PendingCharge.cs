eacf9d9cfc28969bd0fc5cafaa3b222130de48ec
Adam O'Neil <adamosoftware@gmail.com>
2025-03-26 21:31:16 -0400

    progress on pending invoices

---
+++ BlazorApp/Components/Pages/Dashboard/PendingCharge.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Components.Pages.Dashboard;
+
+public class PendingCharge
+{
+	public string Type { get; set; } = default!;
+	public int CustomerId { get; set; }
+	public string CustomerName { get; set; } = default!;
+	public int ProjectId { get; set; }
+	public string ProjectName { get; set; } = default!;
+	public decimal Amount { get; set; }
+}
