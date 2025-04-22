c5ff60194084d77830700fc8ee1c4f907fdc41b9
Adam O'Neil <adamosoftware@gmail.com>
2025-04-22 12:19:41 -0400

    remove fake comment

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -14,8 +14,6 @@ using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// here is a comment with feeling
-
 var defaultCulture = new CultureInfo("en-US");
 CultureInfo.DefaultThreadCurrentCulture = defaultCulture;
 CultureInfo.DefaultThreadCurrentUICulture = defaultCulture;
