f6fa073ca5d3afa657a940d1db87a76e53bde1bd
Adam O'Neil <adamosoftware@gmail.com>
2025-09-03 05:59:20 -0400

    new migration

---
+++ LiteInvoice.Database/Migrations/20250903095642_ScheduledInvoices2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScheduledInvoices2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AutoPostDayOfMonth",
+                table: "Customers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AutoPostTemplateId",
+                table: "Customers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_AutoPostTemplateId",
+                table: "Customers",
+                column: "AutoPostTemplateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Projects_AutoPostTemplateId",
+                table: "Customers",
+                column: "AutoPostTemplateId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Projects_AutoPostTemplateId",
+                table: "Customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_AutoPostTemplateId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "AutoPostDayOfMonth",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "AutoPostTemplateId",
+                table: "Customers");
+        }
+    }
+}
